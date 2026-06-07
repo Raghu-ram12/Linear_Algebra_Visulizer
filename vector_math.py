41cfa5f818d6de0f2f03372b3fbcbfb669d10f32
Raghu-ram12 <raghurampenteala@gmail.com>
2026-06-07 14:22:30 +0530

    add a vector math module

---
+++ vector_math.py
@@ -0,0 +1,55 @@
+import math
+class Vector:
+
+    def __init__(self,components):
+        self.components=components
+        self.dim=len(components)
+    
+def add_vectors(a:Vector,b:Vector)->Vector:
+
+   max_len=max(a.dim,b.dim)
+   result=[]
+   for i in range(max_len):
+        val_a=a.components[i] if a.dim else 0
+        val_b=b.components[i] if b.dim else 0 
+        result.append(val_a+val_b)
+    
+   return Vector(result)
+
+def sub_vectors(a: Vector,b: Vector)->Vector:
+
+   max_len=max(a.dim,b.dim)
+   result=[]
+   for i in range(max_len):
+        val_a=a.components[i] if a.dim else 0
+        val_b=b.components[i] if b.dim else 0 
+        result.append(val_a-val_b)
+    
+   return Vector(result)
+    
+
+def multiply_scaler(a:Vector,k: float)->Vector:
+
+    return Vector([a.components[i]*k for i in range(a.dim)])
+
+def magnitude(a:Vector)->float:
+
+    return sum([a.components[i]**2 for i in range(a.dim)])**0.5
+
+def dot_product(a:Vector,b:Vector)->float:
+ 
+   min_len=max(a.dim,b.dim)
+   result=[]
+   for i in range(min_len):
+        result.append(a.components[i]*b.components[i])
+    
+   return sum(result)
+
+def Vector_angle(a:Vector,b:Vector):
+
+    d=dot_product(a,b)
+    mag_a=magnitude(a)
+    mag_b=magnitude(b)
+    return math.acos(d/(mag_a*mag_b))
+
+ 
