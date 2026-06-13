ba73f20c826c73bae7ef5e0c77249efd9a8e4e10
Raghu-ram12 <raghurampenteala@gmail.com>
2026-06-13 16:53:48 +0530

    reforrtored code and added multi select feature

---
+++ state.py
@@ -0,0 +1,22 @@
+HEIGHT = 800
+WIDTH = 1400
+PANEL_WIDTH = 200
+DRAW_SCALE = 20
+
+canvas_colors = [
+    "crimson",
+    "dodger blue",
+    "forest green",
+    "gold",
+    "dark orchid",
+    "dark orange",
+    "turquoise",
+    "hot pink",
+    "slate gray",
+    "saddle brown",
+]
+
+colour_index = 0
+vector_buttons = []
+button_idx = 0
+draw_scale = DRAW_SCALE
